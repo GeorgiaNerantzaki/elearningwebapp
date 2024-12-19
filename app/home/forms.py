996ef02141d6a74ced27e2b4bf6e1f206a22a187
GeorgiaNerantzaki <86157366+GeorgiaNerantzaki@users.noreply.github.com>
2024-12-19 19:49:42 +0200

    Update forms.py

--- app/home/forms.py
+++ app/home/forms.py
@@ -8,38 +8,24 @@ from wtforms.validators import DataRequired, InputRequired
 from flask_bootstrap5 import Bootstrap
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 
-
-
+#student forms here
+#enrollment to course form
 class Enroll(FlaskForm):
     course_id = HiddenField('Course ID', validators=[DataRequired()])
     submit = SubmitField('Submit')
     
-
+#unenroll button
 class Unenroll(FlaskForm):
     enrollment_id = HiddenField('Enrollment  ID', validators=[DataRequired()])
     submit = SubmitField('Submit')
     
-
+#form for uploading files with solutions for assignment
 class SolutionUploadForm(FlaskForm):
     solution = FileField('File', validators=[FileRequired(), FileAllowed(['pdf', 'docx', 'txt'], 'File types allowed: PDF, DOCX, TXT')])
     submit = SubmitField('Submit')
     
-
+#real time feedback form for questions in the oprional form 
 class FeedbackForm(FlaskForm):
     comment_field = StringField('Any feedback, questions about the lecture,requests etc(your response will be anonymous!).(Note:Anything you submit here must be relevant to the content of lecture not to the course in general!) ')
     submit = SubmitField('Submit')
     
-
-# class LikertForm(FlaskForm):
-#     def __init__(self, lecture_questions, *args, **kwargs):
-#         super(LikertForm, self).__init__(*args, **kwargs)
-
-#         for idx, question in enumerate(lecture_questions, start=1):
-#             field_name = f'question_{idx}'
-#             setattr(self, field_name, RadioField(question, choices=[
-#                 ('5', 'Strongly Agree'),
-#                 ('4', 'Agree'),
-#                 ('3', 'Neutral'),
-#                 ('2', 'Disagree'),
-#                 ('1', 'Strongly Disagree')
-#             ]))
